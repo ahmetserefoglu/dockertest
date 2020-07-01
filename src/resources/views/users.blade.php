558fdefe1cfd79b45ffd95748a5c07be545dfa89
ahmetserefoglu <ahmet.serefoglu46@gmail.com>
2020-07-01 09:47:18 +0300

    newuserstinker

--- src/resources/views/users.blade.php
+++ src/resources/views/users.blade.php
@@ -1,5 +1,24 @@
-@foreach($users as $user)
+@extends('layouts.app')
 
-    <li>{{$user->name}}</li>
-
-@endforeach
+@section('content')
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Email Verify</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach($users as $user)
+    <tr>
+        <th scope="row">{{$user->id}}</th>
+        <th>{{$user->name}}</td>
+        <th>{{$user->email}}</th>
+        <th>{{$user->email_verified_at}}</th>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+@endsection
